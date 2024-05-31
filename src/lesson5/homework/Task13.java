408a99304c3fe3143187846ee687cec0934bccf2
Yury Berahenka <mycantora@gmail.com>
2024-05-31 02:16:40 -0700

    added task 14 corrected task 13

--- src/lesson5/homework/Task13.java
+++ src/lesson5/homework/Task13.java
@@ -2,13 +2,17 @@ package lesson5.homework;
 
 /*Создать последовательность случайных чисел, найти и вывести наибольшее из них*/
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task13 {
+    private static final int MAX_LENGTH_ARRAY = 10;
+    private static final int MIN_LENGTH_ARRAY = 1;
+
     public static void main(String[] args) {
         int[] randomArray = getRandomArray();
         int maxValue = getMaxValue(randomArray);
-        System.out.println(maxValue);
+        System.out.println("Max: " + maxValue);
     }
 
     private static int getMaxValue(int[] randomArray) {
@@ -21,7 +25,7 @@ public class Task13 {
 
     private static int[] getRandomArray() {
         Random random = new Random();
-        int lengthArray = random.nextInt();
+        int lengthArray = random.nextInt(MAX_LENGTH_ARRAY - MIN_LENGTH_ARRAY) + MIN_LENGTH_ARRAY;
         int[] randomArray = new int[lengthArray];
         for (int i = 0; i < randomArray.length; i++) {
             randomArray[i] = random.nextInt();
