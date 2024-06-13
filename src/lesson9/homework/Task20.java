439ea478a5ef40b4d0aafc13e7293e353a0e261a
Yury Berahenka <mycantora@gmail.com>
2024-06-13 02:53:54 -0700

    solution task20

--- src/lesson9/homework/Task20.java
+++ src/lesson9/homework/Task20.java
@@ -1,7 +1,23 @@
 package lesson9.homework;
 
+/*Имеется строка с текстом. Вывести текст, составленный из последних букв
+всех слов.*/
+
 public class Task20 {
     public static void main(String[] args) {
+        String str = " Имеется строка с текстом. Вывести текст, составленный      из последних букв всех слов.    ";
+        String newText = getNewText(str);
+        System.out.println(newText);
+    }
 
+    private static String getNewText(String text) {
+        String[] splitText = text.trim().split("[?!:;,.\\s]");
+        String newText = "";
+        for (String str : splitText) {
+            if (!str.isBlank()) {
+                newText = newText.concat(str.trim().substring(str.length()-1));
+            }
+        }
+        return newText;
     }
 }
