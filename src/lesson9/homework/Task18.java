9778da4f4c4149a1ca914da740e4941d639a42b9
Yury Berahenka <mycantora@gmail.com>
2024-06-13 02:17:04 -0700

    solution task18

--- src/lesson9/homework/Task18.java
+++ src/lesson9/homework/Task18.java
@@ -1,7 +1,18 @@
 package lesson9.homework;
 
+/*Найти в строке не только запятые, но и другие знаки препинания. Подсчитать
+общее их количество.*/
+
+import java.util.Arrays;
+
 public class Task18 {
     public static void main(String[] args) {
+        String str = " Test text: is there 4 punctuation marks here!? or not. ";
+        int countPunctuationMarks = getCountPunctuationMarks(str);
+        System.out.println(countPunctuationMarks);
+    }
 
+    private static int getCountPunctuationMarks(String text) {
+        return text.trim().split("[?!:;,.]").length;
     }
 }
